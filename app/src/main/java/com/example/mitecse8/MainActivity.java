b469c42bff4f12a49449d18535e7c62264acc2b9
ssunilkumar <sunil@mite.ac.in>
2020-09-09 10:07:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mitecse8/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.mitecse8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase database;
+    DatabaseReference myRef;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("message");
+        myRef.setValue("Hello World");
+
+    }
+}
